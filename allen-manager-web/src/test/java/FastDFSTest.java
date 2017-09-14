56ea8476ec6a1c37a80c27bee2db36d813cf0510
weiwosuoai <weiwosuoai1991@gmail.com>
2017-09-14 09:42:52 +0800

    fastdfs 文件上传单元测试

---
+++ allen-manager-web/src/test/java/FastDFSTest.java
@@ -0,0 +1,34 @@
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.StorageServer;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.junit.Test;
+
+/**
+ * fastdfs 文件上传
+ * @author Allen
+ *
+ */
+public class FastDFSTest {
+	
+	@Test
+	public void testUploadFile() throws Exception {
+		ClientGlobal.init("C:/developer/allenmallgit/allen-manager-web/src/main/resources/client.conf");
+		// 创建一个 TrackerClient 对象
+		TrackerClient trackerClient = new TrackerClient();
+		// 使用 trackerClient 对象创建连接，获取一个 trackerServer 对象
+		TrackerServer trackerServer = trackerClient.getConnection();
+		// 创建一个 storageServer 的引用，值为  null
+		StorageServer storageServer = null;
+		StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+		// 使用 storageClient 对象上传图片 Note:拓展名不带 .
+		String[] strs = storageClient.upload_file("C:/Users/Allen/Pictures/timg.jpg", "jpg", null);
+		
+		// 返回数组，包含组名和图片的路径
+		for (String str : strs) {
+			System.out.println(str);
+		}
+	}
+
+}
